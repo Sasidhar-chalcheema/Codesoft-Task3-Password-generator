b14d19d0e2a14e7186c78b815cd621c81fb8db14
Sasidhar Chalcheema <144924136+Sasidhar-chalcheema@users.noreply.github.com>
2024-04-21 13:30:45 +0530

    python.py

---
+++ python.py
@@ -0,0 +1,28 @@
+#PASSWORD GENERATOR
+#task 3
+
+import random
+import string
+
+def generate_password(length, complexity):
+    if complexity == "low":
+        characters = string.ascii_lowercase + string.digits
+    elif complexity == "medium":
+        characters = string.ascii_letters + string.digits
+    elif complexity == "high":
+        characters = string.ascii_letters + string.digits + string.punctuation
+    else:
+        print("Invalid complexity level. Using default (high) complexity.")
+        characters = string.ascii_letters + string.digits + string.punctuation
+    
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+def main():
+    length = int(input("Enter the desired length of the password: "))
+    complexity = input("Enter the complexity level (low/medium/high): ").lower()
+    password = generate_password(length, complexity)
+    print("Generated Password:", password)
+
+if __name__ == "__main__":
+    main()
